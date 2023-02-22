138443737f1ec7e8d94562a2230a0532d2ba2d37
Yuvaraj M <yuvm19is@cmrit.ac.in>
2023-02-22 19:16:05 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //string datatype- it is a sequence of character
+        String MyFirstString="\nps:~ANDROXUS";
+        System.out.println("Death Awaits You All"+MyFirstString);
+        System.out.println("------------------------------------------");
+        MyFirstString=MyFirstString+"\naka THE GODSLAYER";
+        System.out.println("Death Awaits You All"+MyFirstString);
+
+    }
+}
